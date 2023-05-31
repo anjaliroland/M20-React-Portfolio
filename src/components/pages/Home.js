de3d80978502214c69b95d0cc9901eadf3a0a076
anjaliroland <anjaligroland@gmail.com>
2023-05-30 20:01:31 -0700

    centered homepge text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main>
             <div className='homePge'> 
-                <h3 className='text-center'>Welcome to my portfolio!</h3>
+                <h3>Welcome to my portfolio!</h3>
             </div>
         </main>
     )
