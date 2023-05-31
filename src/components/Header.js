aa056abcf70fc6ecb95da431fa1c6ce38adc30be
anjaliroland <anjaligroland@gmail.com>
2023-05-30 19:56:17 -0700

    updated header and nav styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Nav from "./Nav";
+import "../styles/App.css"
 
 export default function Header() {
     return (
-        <div>
-            <header className="container">
-                <h1>ANJALI ROLAND</h1> 
-                <Nav />
-            </header>
-        </div>
+        <header>
+            <h1>ANJALI ROLAND</h1> 
+            <Nav />
+        </header>
     )
 }
