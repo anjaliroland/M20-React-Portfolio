59bc51666963e7926b4912ecf48511d386389eef
anjaliroland <anjaligroland@gmail.com>
2023-05-30 20:07:48 -0700

    changed icon size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ export default function Footer() {
     return (
         <footer className="text-center footer">
             <a href="https://github.com/anjaliroland" target="_blank">
-                <img src={github} alt="github logo" style={{ width: 'auto', height: '8em', }} />
+                <img src={github} alt="github logo" style={{ width: 'auto', height: '6em', }} />
             </a>
             <a href="https://www.linkedin.com/in/anjali-roland/" target="_blank">
-                <img src={linkedin} alt="linkedin logo" style={{ width: 'auto', height: '9em', }} />
+                <img src={linkedin} alt="linkedin logo" style={{ width: 'auto', height: '6em', }} />
             </a>
             <a href="https://open.spotify.com/user/agroland"
                 target="_blank">
-                <img src={spotify} alt="spotify logo" style={{ width: 'auto', height: '10em', }} />
+                <img src={spotify} alt="spotify logo" style={{ width: 'auto', height: '7em', }} />
             </a>
         </footer>
 
